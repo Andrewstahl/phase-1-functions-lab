5348b785d3fcc19b552eb692a440d2281dda2488
Andrew Stahl <andrewstahl96@gmail.com>
2022-04-25 17:01:37 -0400

    commit tree c5eb039082e0bf76335ea879bcf92111e6355865

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+const hqLocation = 42;
+function distanceFromHqInBlocks(pickupBlock) {
+  return Math.abs(hqLocation - pickupBlock);
+}
+
+function distanceFromHqInFeet(pickupBlock) {
+  return distanceFromHqInBlocks(pickupBlock) * 264;
+}
+
+function distanceTravelledInFeet(block1, block2) {
+  return Math.abs(block1 - block2) * 264;
+}
+
+function calculatesFarePrice(block1, block2) {
+  const feetDistance = distanceTravelledInFeet(block1, block2);
+
+  switch (true) {
+    case feetDistance < 400:
+      return 0;
+      break;
+    case feetDistance < 2000:
+      return .02 * (feetDistance - 400);
+      break;
+    case feetDistance < 2500:
+      return 25;
+      break;
+    default:
+      return 'cannot travel that far';
+      break;
+  }
+}
+
+console.log(distanceTravelledInFeet(34, 32));
+console.log(calculatesFarePrice(34, 32));
